e09c9d61c818339bc7bdf93195b154e10b10f9ca
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-12-06 18:06:52 +0200

    transform timer to endless hours

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import moment from "moment";
+// import moment from "moment";
 import { connect } from "react-redux";
 import * as trackerActions from "./tracker.actions";
 
@@ -9,7 +9,7 @@ const Timer = ({ id, name, deleteLap }) => {
   const [isRunning, setIsRunning] = useState(true);
   const [seconds, setSeconds] = useState(0);
 
-  let time = moment().hour(0).minute(0).second(seconds).format('HH : mm : ss');
+  // let time = moment().hour(0).minute(0).second(seconds).format('HH:mm:ss');
 
   useEffect(() => {
     if (isRunning) {
@@ -20,12 +20,23 @@ const Timer = ({ id, name, deleteLap }) => {
     } return undefined;
   }, [isRunning]);
 
+
+  const ss = `${(seconds % 60)}`.slice(-2);
+  const minutes = `${Math.floor(seconds / 60)}`;
+  const mm = `${minutes % 60}`.slice(-2);
+  const hh = `${Math.floor(seconds / 3600)}`.slice(-2);
+
+  let time = (hh < 10 ? '0' + hh : hh) + ':' + (mm < 10 ? '0' + mm : mm) + ':' + (ss < 10 ? '0' + ss : ss);
+
   // debugger;
   return (
     <div className="tracker__list-item">
       <div className="tracker__list">
         <div className="tracker__name">
-          {name + " " + time}
+          {name}
+        </div>
+        <div className="tracker__time">
+          {time}
         </div>
         <div className="tracker__buttons">
           {isRunning
@@ -52,7 +63,6 @@ const mapDispatch = {
   createLap: trackerActions.addLap,
   deleteLap: trackerActions.deleteLap,
 }
-
 
 const ConnectedTimer = connect(null, mapDispatch)(Timer);
 export default ConnectedTimer;
